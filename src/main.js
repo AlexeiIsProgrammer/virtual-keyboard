bb9d7847e30ed685b30d3920da8151091d783369
Alexei <avenge.now.r@mail.ru>
2023-04-30 23:02:08 +0300

    feat: add hover to buttons, add transition to buttons during pressing, print buttons to browser

--- src/main.js
+++ src/main.js
@@ -1,8 +1,185 @@
-const KEYS_ARRAY = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
-                    'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Delete']
+import data from './data/keys.json'
 
-console.log(KEYS_ARRAY);
+let keyboardState = 'caseDown'
 
-window.addEventListener('keyup', (e) => {
-    console.log(e.key)
+async function getKeys () {
+    return data
+}
+
+function resetVisibles () {
+    const allKeyButtons = document.querySelectorAll('.keyboard .key > span > span')
+
+    allKeyButtons.forEach(el => el.classList.remove('active'))
+}
+
+
+function createTextArea() {
+    const textarea = document.createElement('textarea')
+    textarea.className = 'textarea'
+    textarea.rows = 10
+
+    return textarea
+}
+
+function createKeyboard() {
+    const keyboard = document.createElement('div')
+    keyboard.className = 'keyboard'
+
+    return keyboard
+}
+
+function createElement(element) {
+    const createdElement = document.createElement('div')
+    createdElement.className = `key ${element.name}`
+    
+    const rus = document.createElement('span')
+    rus.className = 'rus'
+
+    const caseDown = document.createElement('span')
+    caseDown.className = 'caseDown'
+    caseDown.innerHTML = element.rus.caseDown
+
+    const shiftUp = document.createElement('span')
+    shiftUp.className = 'shiftUp'
+    shiftUp.innerHTML = element.rus.shiftUp
+
+    const caps = document.createElement('span')
+    caps.className = 'caps'
+    caps.innerHTML = element.rus.caps
+
+    const shiftCaps = document.createElement('span')
+    shiftCaps.className = 'shiftCaps'
+    shiftCaps.innerHTML = element.rus.shiftCaps
+
+    rus.append(caseDown, shiftUp, caps, shiftCaps)
+
+    const eng = document.createElement('span')
+    eng.className = 'eng'
+
+    const caseDownEng = document.createElement('span')
+    caseDownEng.className = 'caseDown'
+    caseDownEng.innerHTML = element.eng.caseDown
+
+    const shiftUpEng = document.createElement('span')
+    shiftUpEng.className = 'shiftUp'
+    shiftUpEng.innerHTML = element.eng.shiftUp
+
+    const capsEng = document.createElement('span')
+    capsEng.className = 'caps'
+    capsEng.innerHTML = element.eng.caps
+
+    const shiftCapsEng = document.createElement('span')
+    shiftCapsEng.className = 'shiftCaps'
+    shiftCapsEng.innerHTML = element.eng.shiftCaps
+
+    eng.append(caseDownEng, shiftUpEng, capsEng, shiftCapsEng)
+
+    createdElement.append(rus, eng) 
+
+    return createdElement
+}
+
+function insertAtCursor(myField, myValue) {
+    const myFieldInner = myField
+
+    if (document.selection) {
+        myFieldInner.focus();
+        const sel = document.selection.createRange();
+        sel.text = myValue;
+    }
+    else if (myFieldInner.selectionStart || myFieldInner.selectionStart === '0') {
+        const startPos = myFieldInner.selectionStart;
+        const endPos = myFieldInner.selectionEnd;
+        myFieldInner.value = myFieldInner.value.substring(0, startPos)
+            + myValue
+            + myFieldInner.value.substring(endPos, myFieldInner.value.length);
+
+    } else {
+        myFieldInner.value += myValue;
+    }
+
+
+}
+
+
+let language = 'rus' 
+
+function fillState () {
+    resetVisibles()
+
+    const letters = document.querySelectorAll(`.${language} .${keyboardState}`)
+    letters.forEach(el => el.classList.add('active'))
+}
+
+getKeys().then( keysArr => {
+    const textarea = createTextArea()
+
+    textarea.addEventListener('keydown', (e) => {
+        e.preventDefault()
+    })
+
+    const keyboard = createKeyboard()
+
+    keysArr.forEach(keyElement => {
+        keyboard.append(createElement(keyElement))
+    })
+
+    document.body.append(textarea, keyboard)
+
+    fillState()
+
+    window.addEventListener('keydown', (e) => {
+
+        e.preventDefault()
+
+        const elementName = document.querySelector(`.${e.code}`)
+        elementName.classList.add('hovered')
+    
+        if(e.key === 'Control' && e.key === 'Alt') {
+            console.log('Смена');
+            language = language === 'rus' ? 'eng' : 'rus'
+            fillState()
+    
+            return
+        }
+    
+        if (e.code === 'CapsLock') {
+    
+            if(keyboardState === 'caps') {
+                keyboardState = 'caseDown'
+            } else if (keyboardState === 'shiftUp') {
+                keyboardState = 'shiftCaps'
+            }
+            else {
+                keyboardState = 'caps'
+            }
+            
+            fillState()
+        }
+    
+        if (e.key === 'Shift' && keyboardState !== 'shiftCaps') {
+            keyboardState = keyboardState === 'caps' ? 'shiftCaps' : 'shiftUp'
+            fillState()
+        }
+
+        //  Вставка значения
+
+        const insertedValue = e.key
+
+        insertAtCursor(textarea, insertedValue)
+    })
+    
+    window.addEventListener('keyup', (e) => {
+        const elementName = document.querySelector(`.${e.code}`)
+        elementName.classList.remove('hovered')
+
+        if (e.key === 'Shift') {
+            keyboardState = keyboardState === 'shiftCaps' ? 'caps' : 'caseDown'
+            fillState()
+        }
+    
+    })
+
+}).catch(e => {
+    console.log(e);
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,39 @@
-const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //Плагин для HTLM npm install html-webpack-plugin -D
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //Плагин для CSS npm install mini-css-extract-plugin -D
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-const mode = process.env.NODE_ENV || 'development'; //указание среды разработки ()
 
-const devMode = mode === 'development'; // Проверка мода на девелопмент
+const postcss = require('postcss-preset-env')
 
-const target = devMode ? 'web' : 'browserslist'; // Если development, то web настройки, если продакшн, то файл browserslistrc
-const devtool = devMode ? 'source-map' : undefined; // При дев моде, будет source-map для отслежиания ошибок 
+const mode = process.env.NODE_ENV || 'development';
+
+const devMode = mode === 'development';
+
+const target = devMode ? 'web' : 'browserslist'; 
+const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
-    mode, //Мод (дев или прод)
-    target, // ресурс (web или browserslistrc)
-    devtool, // отслеживание ошибок
+    mode,
+    target,
+    devtool,
     devServer: {
-        port: 3000, //Порт
-        open: true, //Открывать браузер //Обновление стилей (бывают сложности)
+        port: 3000,
+        open: true,
     },
-    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')], //npm install --save @babel/polyfill : для поддержки скриптов из браузеров
+    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
     output: {
         path: path.resolve(__dirname, 'dist'),
         clean:true,
-        filename: 'index.js', //name - main по умолчанию, contenthash нужен для сборки разных имен, чтобы обновление было из разных файлов
+        filename: 'index.js',
     },
     module: {
         rules: [
-            { //npm install --save-dev html-loader
+            {
                 test: /\.html$/i,
                 loader: 'html-loader',
             },
-            {//npm install --save-dev @babel/core @babel/preset-env babel-loader (Бабель)
+            {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
@@ -38,16 +41,16 @@ module.exports = {
                     presets: ['@babel/preset-env'],
                 }
             },
-            {//npm install --save-dev css-loader sass-loader sass (Сассик)
+            {
                 test: /\.(c|sc|sa)ss$/i,
                 use: [
                     devMode ? "style-loader" : MiniCssExtractPlugin.loader,
                     "css-loader",
-                    { // npm install --save-dev postcss-loader postcss postcss-preset-env (Префиксы)
+                    {
                         loader: 'postcss-loader',
                         options: {
                             postcssOptions:  {
-                                plugins: [require('postcss-preset-env')],
+                                plugins: [postcss],
                             }
                         }
                     },
@@ -55,7 +58,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(svg|jpg|jpeg|png|gif)$/,
+                test: /\.(svg|jpg|jpeg|png|gif|ico)$/,
                 type: 'asset/resource',
             }
         ]
@@ -73,5 +76,3 @@ module.exports = {
 
     ]
 };
-
-//npm i scss-reset --save; почистить html
