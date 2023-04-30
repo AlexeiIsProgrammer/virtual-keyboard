bffd7be10fdb27957188dcde3007043308163ac6
Alexei <avenge.now.r@mail.ru>
2023-04-30 20:29:42 +0300

    feat: fill database partlicular

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const KEYS_ARRAY = ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace',
+                    'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Delete']
+
+console.log(KEYS_ARRAY);
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key)
+})
